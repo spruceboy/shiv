0a0373029cd23d148e889cbc1e15f2a16b01530e
jay cable <jay@alaska.edu>
2009-04-16 12:22:37 -0800

    Start script, for deplyoment.

--- start.bash
+++ start.bash
@@ -1,3 +1,4 @@
+export PATH=$PATH:/usr/local/lib/ruby/gems/1.8/bin/
 screen -S shiv-prod-3333 -d -m rackup -E deployment -p 3333 -s mongrel shiv.ru ./log_one/
 screen -S shiv-prod-3334 -d -m rackup -E deployment -p 3334 -s mongrel shiv.ru ./log_two/
 
