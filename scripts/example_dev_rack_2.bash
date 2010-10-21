55b451cc6afbbae9eaf7e26a797023d0b84906b0
JC <jc@alaska.edu>
2010-10-21 14:49:26 -0800

    Massive reshuffle to cleanup config files and to make examples more user friendy.
    Signed-off-by: JC <jc@alaska.edu>

--- scripts/example_dev_rack_2.bash
+++ scripts/example_dev_rack_2.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
+# An example "shiv" start script, for one "shiv" instance.
+# setup the enviroment..
 . $HOME/.bashrc
+# start shiv
 rackup --eval "log_dir='./logs2/'" -E deployment -p 5557 -s mongrel shiv.ru 
-
-
--- scripts/snap_start.bash
+++
@@ -1,3 +0,0 @@
-screen -S shiv-prod-5555 -d -m rackup --eval "log_dir='./logs/'" -E deployment -p 5555 -s mongrel shiv.ru ./log_one/
-
-
