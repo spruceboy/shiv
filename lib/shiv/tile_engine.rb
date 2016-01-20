257589a1fb6807b75aaa8b1a1bdd65dbb6d533ae
JC <jay@alaska.edu>
2016-01-20 10:27:36 -0900

    Split out locking stuff, added a in_progress to check to see if a particular request is in progress.
    Signed-off-by: JC <jay@alaska.edu>

--- lib/shiv/tile_engine.rb
+++ lib/shiv/tile_engine.rb
@@ -5,6 +5,7 @@ require 'shiv/idler'
 require 'shiv/storage_engine'
 require 'shiv/tile_mapper'
 require 'shiv/http_client_tools'
+require 'shiv/locker'
 ###
 # Notes
 #                     **** tile Layout ****************
@@ -30,6 +31,7 @@ class TileEngine
     @log = logger
     @storage_format = cfg['storage_format']
     @requests = {}
+    @locker = TileLockerFile.new(@log)
 
     # Save some stuff for easy (shorter typing) access later..
     @x_size = cfg['tiles']['x_size']
@@ -68,6 +70,18 @@ class TileEngine
   # check that x,y,z are valid.
   def valid?(x, y, z)
     @tile_mapper.valid?(x, y, z)
+  end
+
+  ##
+  # checks to see if a tile is being generated. 
+  def in_progress(x,y,z) 
+
+	if ( @tile_mapper.single?(x, y, z) )
+		return @locker.is_locked?(x,y,z)
+	else
+		xs,ys = shift_x_y(x,y) 
+		return @locker.is_locked?(xs,ys,z)
+	end
   end
 
   ##
@@ -304,129 +318,5 @@ class ExternalTileEngine < TileEngine
     (-1).upto(1) { |dx| (-1).upto(1) { |dy| @@idler.add(self, x + dx * @cfg['tiles']['x_count'], y + dy * @cfg['tiles']['y_count'], z) unless dy == 0 && dx == 0 } }
 
     make_tiles(x, y, z)
-  end
-end
-
-##
-# A per tile clocking sceme... fun for all..
-
-class TileLocker
-  def initialize(log)
-    @log = log
-    @locker = {}
-    @wait = 0.3
-    @m_lock = Mutex.new
-    @lt = 'TileLocker'
-  end
-
-  ###
-  # This is confusing.. pay attention!
-  # This function is used to control requests, if a request is in progress, it waits until the request is finished, then returns true, with the lock not set/altered.
-  # If a request is not in progress, it returns false and sets the lock.
-  def check_and_wait(x, y, z)
-    busy = false
-    until  check_lock(x, y, z)
-      @log.msgdebug(@lt + "check_and_wait -> waiting on #{x},#{y},#{z}")
-      sleep(@wait)
-      busy = true
-    end
-
-    release_lock(x, y, z) if busy # Release lock, and data has allready been generated..
-    busy
-  end
-
-  # Returns true if locked, false otherwise..
-  def check_lock(x, y, z)
-    token = "#{x}_#{y}_#{z}"
-    @m_lock.synchronize do
-      if !@locker[token]
-        @log.msgdebug(@lt + "locking on #{x},#{y},#{z}")
-        @locker[token] = Time.now
-        return true
-      else
-        return false
-      end
-    end
-  end
-
-  # to be called after check_and_wait..
-  def release_lock(x, y, z)
-    token = "#{x}_#{y}_#{z}"
-    @m_lock.synchronize { @locker.delete(token) }
-  end
-end
-
-class TileLockerFile
-  WAIT_TIME = (60 * 3)
-  def initialize(log)
-    @log = log
-    @lock_dir = './locks/'
-    @wait = 0.3
-    @m_lock = Mutex.new
-    @lt = 'TileLockerFile:'
-  end
-
-  ###
-  # This is confusing.. pay attention!
-  # This function is used to control requests, if a request is in progress, it waits until the request is finished, then returns true, with the lock not set/altered.
-  # If a request is not in progress, it returns false and sets the lock.
-  # Ok, I lied, it should always return with a lock in place..
-  def check_and_wait(x, y, z)
-    busy = false
-    until  check_lock(x, y, z)
-      @log.msgdebug(@lt + "check_and_wait -> waiting on #{x},#{y},#{z}")
-      sleep(@wait)
-      busy = true
-    end
-    busy
-  end
-
-  # Returns true if locked, false otherwise..
-  def check_lock(x, y, z)
-    if !locked(x, y, z)
-      @log.msgdebug(@lt + "locking on #{x},#{y},#{z}")
-      return true
-    else
-      return false
-    end
-  end
-
-  # to be called after check_and_wait..
-  def release_lock(x, y, z)
-    @log.msgdebug(@lt + "releasing #{x},#{y},#{z}")
-    @m_lock.synchronize do
-      begin
-        File.delete(getpath(x, y, z))
-      rescue => e
-        @log.msgerror(@lt + "release lock -> colision at #{x},#{y},#{z} (#{e})")
-      end
-    end
-  end
-
-  def getpath(x, y, z)
-    ("#{@lock_dir}#{x}_#{y}_#{z}")
-  end
-
-  def locked(x, y, z)
-    @m_lock.synchronize do
-      path = getpath(x, y, z)
-
-      begin # this section deals with old locks - if lock file exists and is old, delete..
-        if  File.exist?(path) && ((Time.now - File.mtime(path)) > WAIT_TIME)
-          @log.msgerror(@lt + "Lock timeout on #{x},#{y},#{z}  ")
-          File.delete(path)
-        end
-      rescue
-        # Do nothing - means file is gone.
-      end
-
-      # Normal path
-      if File.exist?(path)
-        return true
-      else
-        File.open(path, 'w') { |fl| fl.write(Time.now.to_s); fl.flush }
-        return false
-      end
-    end
   end
 end
